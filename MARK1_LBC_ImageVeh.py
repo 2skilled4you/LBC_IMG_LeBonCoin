ae9ae7e1bebeff39d2cc87c46e8e97276b604b90
Jonathan Petit <petit.jonathan@live.fr>
2020-04-15 16:23:55 +0200

    Update MARK1_LBC_ImageVeh.py

--- MARK1_LBC_ImageVeh.py
+++ MARK1_LBC_ImageVeh.py
@@ -13,15 +13,6 @@ import linecache
 import sys
 from subprocess import Popen, PIPE, STDOUT, check_output
 
-#strFullURLName = "C:/Users/petitbrandao/Documents/Python Scripts/03. Ebay Allianz/Reverse Tarif Allianz.xlsx"
-#wbEbay = load_workbook(filename=strFullURLName)
-#
-#print(wbAPRIL.get_sheet_names())
-#
-#wsEbay = wbEbay.get_sheet_by_name('Data')
-#wsParametres = wbAPRIL.get_sheet_by_name('Paramètres')
-#print(wsGroupama['A1'].value)
-#print(wsGroupama['A' + str(1)].value)
 ############################################################
 ######## FONCTIONS UTILES AU DEROULEMENT DU SCRIPT #########
 ############################################################
@@ -66,15 +57,12 @@ def check_exists_by_xpath(xpath):
         return False
     return True
 
-py_process = Popen([r'C:\Users\Bernard\Documents\Python Scripts\60. Outils\clearcachePACIFICA.bat'], stdout=PIPE, stderr=STDOUT)
+py_process = Popen([r'C:\Users\Bernard\Documents\Python Scripts\60. Outils\clearcache.bat'], stdout=PIPE, stderr=STDOUT)
 
 time.sleep(5)
-#subprocess.call([r'C:\Users\Bernard\Documents\Python Scripts\80. Git\LauchChromeExperimental.bat'])
-#            OpenChromeExp = Popen('cmd')
-#            Popen('"C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9223 --user-data-dir="C:\selenium\AutomationProfile" "http://127.0.0.1:9223"')
-py_process = Popen([r'C:\Users\Bernard\Documents\Python Scripts\60. Outils\LaunchChromeExperimentalPACIFICA.bat'], stdout=PIPE, stderr=STDOUT)
 
-#driver = webdriver.Firefox()
+py_process = Popen([r'C:\Users\Bernard\Documents\Python Scripts\60. Outils\LaunchChromeExperimental.bat'], stdout=PIPE, stderr=STDOUT)
+
 chrome_driver = "C:\\Users\\Public\\Documents\\Python Scripts\\chromedriver.exe"
 chrome_options = Options()
 chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9225")
@@ -82,8 +70,6 @@ chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9225")
 time.sleep(10)
             
 driver = webdriver.Chrome(chrome_driver, chrome_options=chrome_options)
-
-#driver = webdriver.Chrome("C:\\Users\\Bernard\\chromedriver.exe")
 
 driver.get("https://www.leboncoin.fr/voitures/offres/")
 
@@ -94,9 +80,6 @@ for eachButton in driver.find_elements_by_css_selector("button"):
     if eachButton.get_attribute("innerText") == "J’ai compris":
         eachButton.click()
         
-#inpPRO = driver.find_element_by_xpath("//div[@data-qa-id='pro_filter']/div/label/span[2]")
-#inpPRO.click()
-
 time.sleep(5)
 
 strImgPath = "C:/Users/Public/Documents/Drive/04. Reconnaissance d'image - Immatriculations/02. Collecte/02. ImageVeh/"
@@ -123,8 +106,7 @@ for i in range(0, 1):
     time.sleep(60)
     driver.refresh()
     time.sleep(10)
-#labelAnnonce = inpAnnonce.get_attribute("innerText")
-#print(labelAnnonce)
+
 
 
 
